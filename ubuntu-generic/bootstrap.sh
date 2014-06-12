4919d5bdb16cc9ff2f69946b19df13fbf6633ed4
Jean Carlo Machado <j34nc4rl0@gmail.com>
2014-06-12 17:06:08 -0300

    mensagem automática jean - Thu Jun 12 17:06:04 BRT 2014

---
+++ ubuntu-generic/bootstrap.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+php_ini_file="/etc/php5/apache2/php.ini"
+
+[ ! -d /projects ] && {
+
+	mkdir /projects
+	chmod 777 -R /projects
+
+}
+
+#atualiza as listas de pacotes
+apt-get update 
+#for add-apt-repository connand on 12.04
+sudo apt-get -y -q install python-software-properties
+#add lastest php repository
+sudo add-apt-repository -y ppa:ondrej/php5
+apt-get update 
+
+#configurações do phpmyadmin
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password root' | debconf-set-selections
+
+#instala o servidor
+apt-get install -y -q apache2 php5 php5-cli mysql-client mysql-server php5-mysql 
+
+apt-get install -y -q phpmyadmin 
+
+#aditional useful packages
+apt-get install -y -q git lynx vim aptitude
+
+mysqladmin -u root password root	
+
+rm  -rf /etc/apache2/sites-enabled/*
+
+
+[ ! -e /etc/apache2/sites-enabled/virtual.conf ] && {
+
+	ln -s /vagrant/vhosts.conf /etc/apache2/sites-enabled/virtual.conf
+}
+	
+[ -e /etc/hosts.bkp ] && {
+	mv -f /etc/hosts.bkp /etc/hosts
+}
+
+cat /vagrant/hosts >> /etc/hosts
+cp /etc/hosts /etc/hosts.bkp
+
+
+#enables mod rewrite
+[ ! -e  /etc/apache2/mods-enabled/rewrite.load ] && {
+
+	ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
+}
+
+#creates the default website on root 
+[ ! -d /projects/tests ] && {
+
+	mkdir /projects/tests
+}
+
+
+[ -e "$php_ini_file.bkp" ] && {
+	mv -f "$php_ini_file.bkp" $php_ini_file
+}
+
+cat /vagrant/phpini >> $php_ini_file
+cp $php_ini_file "$php_ini_file.bkp"
+
+
+echo "<?php phpinfo(); " > /projects/tests/index.php
+
+update-rc.d apache2 defaults
+update-rc.d mysql defaults
+
+
+
+sudo apache2ctl restart
+service mysql restart
